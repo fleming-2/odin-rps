00539de590a73f1208ecfc70dad4fd3b6a28f1b9
Liam Fleming <fleming.squared@proton.me>
2024-06-16 03:24:09 +0100

    Add ability to get the human player's choice

--- game.js
+++ game.js
@@ -10,3 +10,18 @@ function getComputerChoice() {
         return "scissors";
     }
 }
+
+// Get a choice as a string from the human player
+function getHumanChoice() {
+    let choice;
+    let promptMessage = "Enter your choice:"
+
+    do {
+        choice = prompt(promptMessage);
+        choice = choice.trim().toLowerCase();
+
+        promptMessage = "Your choice must be one of rock, paper, or scissors:"
+    } while (choice != "rock" && choice != "paper" && choice != "scissors");
+
+    return choice;
+}
