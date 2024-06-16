0d2d34a8cef9f7c921f3ad591ccb44d19e6af937
Liam Fleming <fleming.squared@proton.me>
2024-06-16 04:18:06 +0100

    Implement game logic

--- game.js
+++ game.js
@@ -21,7 +21,67 @@ function getHumanChoice() {
         choice = choice.trim().toLowerCase();
 
         promptMessage = "Your choice must be one of rock, paper, or scissors:"
-    } while (choice != "rock" && choice != "paper" && choice != "scissors");
+    } while (choice !== "rock" && choice !== "paper" && choice !== "scissors");
 
     return choice;
+}
+
+
+// Returns the choice that would beat another
+function beats(choice) {
+    switch (choice) {
+        case "scissors":
+            return "rock";
+        case "rock":
+            return "paper";
+        case "paper":
+            return "scissors";
+    }
+}
+
+
+
+// --- Core game logic ---
+
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+
+    function playRound(humanChoice, computerChoice) {
+        let message = "invalid input"; // Indicates no condition satisfied
+
+        if (computerChoice == humanChoice) {
+            message = "Tie!";
+        }
+        else if (computerChoice === beats(humanChoice)) {
+            message = `You lose! ${computerChoice} beats ${humanChoice}.`;
+            computerScore++;
+        }
+        else if (humanChoice === beats(computerChoice)) {
+            message = `You win! ${humanChoice} beats ${computerChoice}.`;
+            humanScore++;
+        }
+        console.log(message);
+    }
+
+    // Play 5 rounds
+    for (let i = 0; i < 5; i++) {
+        let humanSelection = getHumanChoice();
+        let computerSelection = getComputerChoice();
+
+        playRound(humanSelection, computerSelection);
+    }
+
+    // Declare the winner
+    let endMessage = "All rounds played. "
+    if (humanScore > computerScore) {
+        endMessage += "The human wins!"
+    }
+    else if (computerScore > humanScore) {
+        endMessage += "The computer wins!"
+    }
+    else {
+        endMessage += "The game ends with a tie!"
+    }
+    console.log(endMessage);
 }
